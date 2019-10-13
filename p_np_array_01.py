ce3e7b95e70c155a0345565e40b7896764f00b52
tsksnhr <toshiki.o3o5.s@gmail.com>
2019-10-13 13:14:29 +0900

    implementation numpy100 up to 20

---
+++ p_np_array_01.py
@@ -0,0 +1,111 @@
+#1 Import the numpy package under the name np
+import numpy as np
+
+#2 Print the numpy version and the configuration
+print(np.__version__)
+np.show_config()
+
+#3 Create a null vector of size 10
+z_ = np.zeros(10)
+print(z_)
+
+#4 How to find the memory size of any array
+# miss
+# A = np.arange(1,10)
+# print(A)
+# print(np.size(A))
+A = np.zeros((10,10))
+print("{} bites".format(A.size*A.itemsize))
+
+#5 How to get the documentation of the numpy add function from the command line?
+# %run 'python -c "import numpy; numpy.info(numpy.add)"'
+
+#6 Create a null vector of size 10 but the fifth value which is 1
+z_ = np.zeros(10)
+z_[4] = 1
+print(z_)
+
+#7 Create a vector with values ranging from 10 to 49
+v_ = np.arange(10,50)
+print(v_)
+
+#8 Reverse a vector (first element becomes last)
+v_ = v_[::-1]
+print(v_)
+
+#9 Create a 3x3 matrix with values ranging from 0 to 8
+A = np.arange(0,9).reshape(3,3)
+print(A)
+
+#10 Find indices of non-zero elements from [1,2,0,0,4,0]
+A = np.array([1,2,0,0,4,0])
+A = A[A != 0]
+print(A)
+
+#11 Create a 3x3 identity matrix
+A = np.eye(3)
+print(A)
+
+#12 Create a 3x3x3 array with random values
+# miss
+# A = np.random.randint(1,9,27)
+# A = A.reshape(3,3,3)
+# print(A)
+
+A = np.random.random((3, 3, 3))
+print(A)
+
+#13 Create a 10x10 array with random values and find the minimum and maximum values
+# miss
+# A = np.random.random((10,10))
+# A = A[max(A) and min(A)]
+# print(A)
+
+A = np.random.random((10,10))
+Amax, Amin = A.max(), A.min()
+print(Amax, Amin)
+
+#15  Create a random vector of size 30 and find the mean value
+v_ = np.random.random(30)
+v_mean = v_.mean()
+print(v_mean)
+
+#16 Create a 2d array with 1 on the border and 0 inside
+# miss
+# A = np.ones((3,3))
+# A[1, 1] = 0
+# print(A)
+
+A = np.ones((10,10))
+A[1:-1,1:-1] = 0
+print(A)
+
+#17 What is the result of the following expression?
+print(0 * np.nan)
+print(np.nan == np.nan)
+print(np.inf > np.nan)
+print(np.nan - np.nan)
+print(np.nan in set([np.nan]))
+print(0.3 == 3 * 0.1)
+
+#18 Create a 5x5 matrix with values 1,2,3,4 just below the diagonal
+A = np.zeros((5,5))
+for i in range(1,5):
+    A[i,i-1] = i
+print(A)
+
+#19 Create a 8x8 matrix and fill it with a checkerboard pattern
+A = np.zeros((8,8))
+for i in range(0,8):
+    if i%2 == 0:
+        for j in range(0,7,2):
+            A[i,j] = 1
+    else:
+        for k in range(1,9,2):
+            A[i,k] = 1
+print(A)
+
+#20 Consider a (6,7,8) shape array, what is the index (x,y,z) of the 100th element?
+A = np.arange(0,6*7*8)
+A = A.reshape(6,7,8)
+print(A)
